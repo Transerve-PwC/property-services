897d8a2c16c4c8a42ed1dc19b348de05d4df87ed
Rajiv Narayana <rajiv.s@transerve.com>
2020-06-25 15:20:12 +0530

    Fix unit tests. Use latest request json

--- cs-property-service/src/test/java/org/egov/cpt/config/TestConfiguration.java
+++ cs-property-service/src/test/java/org/egov/cpt/config/TestConfiguration.java
@@ -1,10 +1,25 @@
 package org.egov.cpt.config;
 
+import org.egov.cpt.producer.Producer;
+import org.egov.cpt.workflow.WorkflowIntegrator;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 @Profile("test")
 @Configuration
 public class TestConfiguration {
-
+	@Bean
+    @Primary
+	 public WorkflowIntegrator workflowIntegrator(){
+    	return Mockito.mock(WorkflowIntegrator.class);
+    }
+	
+	@Bean
+    @Primary
+	 public Producer producer(){
+    	return Mockito.mock(Producer.class);
+    }
 }
--- cs-property-service/src/test/java/org/egov/cpt/service/PropertyServiceValidationTest.java
+++ cs-property-service/src/test/java/org/egov/cpt/service/PropertyServiceValidationTest.java
@@ -20,11 +20,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@ActiveProfiles("test")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = CSPropertyApplication.class)
 public class PropertyServiceValidationTest {
