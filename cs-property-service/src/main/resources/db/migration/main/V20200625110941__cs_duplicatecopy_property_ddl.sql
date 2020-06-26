f392b05add44671916eb43a96f91c84c40d0b259
sunitha <sunitha.ranjith@transerve.com>
2020-06-26 15:37:16 +0530

    modification in DB Table name

--- cs-property-service/src/main/resources/db/migration/main/V20200625110941__cs_duplicatecopy_property_ddl.sql
+++ cs-property-service/src/main/resources/db/migration/main/V20200625110941__cs_duplicatecopy_property_ddl.sql
@@ -1,12 +1,12 @@
-DROP TABLE IF EXISTS cs_pt_duplicatecopy_property_v1;
-DROP TABLE IF EXISTS cs_pt_duplicatecopy_applicant_v1;
 DROP TABLE IF EXISTS cs_pt_duplicate_ownership_application;
-DROP TABLE IF EXISTS cs_pt_duplicatecopy_property_audit_v1;
-DROP TABLE IF EXISTS cs_pt_duplicatecopy_applicant_audit_v1;
+DROP TABLE IF EXISTS cs_pt_duplicatecopy_applicant;
+DROP TABLE IF EXISTS cs_pt_duplicate_ownership_application;
+DROP TABLE IF EXISTS cs_pt_duplicate_ownership_application_audit;
+DROP TABLE IF EXISTS cs_pt_duplicatecopy_applicant_audit;
 
 --> Duplicate copy Property table
 
-CREATE TABLE cs_pt_duplicatecopy_property_v1 (
+CREATE TABLE cs_pt_duplicate_ownership_application (
    id           			CHARACTER VARYING (256) NOT NULL,
    transit_number       	CHARACTER VARYING (256) NOT NULL,
    tenantid			    	CHARACTER VARYING (256),
@@ -18,12 +18,12 @@ CREATE TABLE cs_pt_duplicatecopy_property_v1 (
    modified_by     			CHARACTER VARYING (128),
    modified_time       		bigint,
 
-  CONSTRAINT pk_cs_pt_duplicatecopy_property_v1 PRIMARY KEY (id),
-  CONSTRAINT fk_cs_pt_duplicatecopy_property_v1 FOREIGN KEY (transit_number) REFERENCES cs_pt_property_v1 (transit_number)  
+  CONSTRAINT pk_cs_pt_duplicate_ownership_application PRIMARY KEY (id),
+  CONSTRAINT fk_cs_pt_duplicate_ownership_application FOREIGN KEY (transit_number) REFERENCES cs_pt_property_v1 (transit_number)  
 );
 
 --> Duplicate copy applicant details table
-CREATE TABLE cs_pt_duplicatecopy_applicant_v1 (
+CREATE TABLE cs_pt_duplicatecopy_applicant (
    id           		CHARACTER VARYING (256) NOT NULL,
    property_id       	CHARACTER VARYING (256),
    tenantid			    CHARACTER VARYING (256),
@@ -39,30 +39,31 @@ CREATE TABLE cs_pt_duplicatecopy_applicant_v1 (
    modified_by     		CHARACTER VARYING (128),
    modified_time       	bigint,
 
-  CONSTRAINT pk_cs_pt_duplicatecopy_applicant_v1 PRIMARY KEY (id),
-  CONSTRAINT fk_cs_pt_duplicatecopy_applicant_v1 FOREIGN KEY (property_id) REFERENCES cs_pt_duplicatecopy_property_v1 (id)
+  CONSTRAINT pk_cs_pt_duplicatecopy_applicant PRIMARY KEY (id),
+  CONSTRAINT fk_cs_pt_duplicatecopy_applicant FOREIGN KEY (property_id) REFERENCES cs_pt_duplicate_ownership_application (id)
   ON UPDATE CASCADE
   ON DELETE CASCADE
 );
 
 --> Duplicate copy document table
-CREATE TABLE cs_pt_duplicate_ownership_application(
+CREATE TABLE cs_pt_duplicatecopy_document(
     id 					CHARACTER VARYING(64),
     tenantId 			CHARACTER VARYING(64),
     documentType 		CHARACTER VARYING(64),
     filestoreid 		CHARACTER VARYING(64),
     propertydetailid 	CHARACTER VARYING(64),
     active 				BOOLEAN,
+    
     createdBy 			CHARACTER VARYING(64),
     lastModifiedBy 		CHARACTER VARYING(64),
     created_time 		bigint,
     modified_time 		bigint,
 
-    CONSTRAINT uk_cs_pt_duplicate_ownership_application PRIMARY KEY (id),
-    CONSTRAINT fk_cs_pt_duplicate_ownership_application FOREIGN KEY (propertydetailid) REFERENCES cs_pt_propertydetails_v1 (id)
+    CONSTRAINT uk_cs_pt_duplicatecopy_document PRIMARY KEY (id),
+    CONSTRAINT fk_cs_pt_duplicatecopy_document FOREIGN KEY (propertydetailid) REFERENCES cs_pt_propertydetails_v1 (id)
 );
 
-CREATE TABLE cs_pt_duplicatecopy_property_audit_v1 (
+CREATE TABLE cs_pt_duplicate_ownership_application_audit(
    id           			CHARACTER VARYING (256) NOT NULL,
    transit_number       	CHARACTER VARYING (256) NOT NULL,
    tenantid			    	CHARACTER VARYING (256),
@@ -75,13 +76,13 @@ CREATE TABLE cs_pt_duplicatecopy_property_audit_v1 (
    modified_time       		bigint
 );
 
-CREATE TABLE cs_pt_duplicatecopy_applicant_audit_v1 (
+CREATE TABLE cs_pt_duplicatecopy_applicant_audit (
    id           		CHARACTER VARYING (256) NOT NULL,
    property_id       	CHARACTER VARYING (256),
    tenantid			    CHARACTER VARYING (256),
    name       			CHARACTER VARYING (256),
    email       			CHARACTER VARYING (256),
-   mobileno       			CHARACTER VARYING (256),
+   mobileno       		CHARACTER VARYING (256),
    guardian				CHARACTER VARYING (256),
    relationship			CHARACTER VARYING (256),
    address				CHARACTER VARYING (256),
