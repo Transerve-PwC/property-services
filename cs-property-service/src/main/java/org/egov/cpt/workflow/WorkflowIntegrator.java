2140a93c49a9447926cc258eee916a6ba3963269
shivamgaba <shivam.gaba@transerve.com>
2020-07-28 17:39:50 +0530

    document list added in owner model and workflowintegreter

--- cs-property-service/src/main/java/org/egov/cpt/workflow/WorkflowIntegrator.java
+++ cs-property-service/src/main/java/org/egov/cpt/workflow/WorkflowIntegrator.java
@@ -297,7 +297,7 @@ public class WorkflowIntegrator {
 			obj.put(MODULENAMEKEY, MODULENAMEVALUE);
 			obj.put(AUDITDETAILSKEY, owner.getAuditDetails());
 			obj.put(COMMENTKEY, owner.getComment());
-
+			obj.put(DOCUMENTSKEY, owner.getWfdocuments());
 			if (!CollectionUtils.isEmpty(owner.getAssignee())) {
 				if (assigneeUuidmaps.size() == 1) {
 					obj.put(ASSIGNEEKEY, assigneeUuidmaps.get(0));
