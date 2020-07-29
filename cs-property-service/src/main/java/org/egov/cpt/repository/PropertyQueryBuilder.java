715c5cdff24ceccfdec4a5f5afbea8979e5874f0
vinil-transerve <66253904+vinil-transerve@users.noreply.github.com>
2020-07-29 22:31:22 +0530

    Bug/ot pi app num (#80)
    * conflicts resolved
    
    * ot,pi application number bug resolved
    
    * minor changes

--- cs-property-service/src/main/java/org/egov/cpt/repository/PropertyQueryBuilder.java
+++ cs-property-service/src/main/java/org/egov/cpt/repository/PropertyQueryBuilder.java
@@ -40,7 +40,7 @@ public class PropertyQueryBuilder {
 			+ " od.id as odid, od.property_id as odproperty_id," + " od.owner_id odowner_id, od.tenantid as odtenantid,"
 			+ " od.name, od.email, od.phone," + " od.gender, od.date_of_birth, od.aadhaar_number,"
 			+ " od.allotment_startdate, od.allotment_enddate," + " od.posession_startdate, od.posession_enddate,"
-			+ " od.monthly_rent, od.revision_period, od.revision_percentage, od.father_or_husband, od.relation,"
+			+ " od.monthly_rent, od.revision_period, od.revision_percentage, od.father_or_husband, od.relation, od.application_number as odapplication_number"
 
 			+ " address.id as aid, address.property_id as aproperty_id, address.transit_number as atransit_number,"
 			+ " address.tenantid as atenantid, address.colony, address.area as addressArea, address.district,"
@@ -49,20 +49,19 @@ public class PropertyQueryBuilder {
 			+ " doc.id as docid, doc.property_id as docproperty_id, doc.tenantid as doctenantid,"
 			+ " doc.is_active as docis_active, doc.document_type, doc.fileStore_id, doc.document_uid,"
 			+ " doc.created_by as dcreated_by, doc.created_date as dcreated_date, doc.modified_by as dmodified_by, doc.modified_date as dmodified_date,"
-			
+
 			+ " pi.id as piid, pi.propertyid as pipropertyid, pi.tenantid as pitenantid, pi.application_number as piapp_number, pi.description as pidescription,"
 			+ "pi.created_by as piCreatedBy, pi.created_time as piCreatedTime, pi.modified_by as piModifiedBy, pi.modified_time as piModifiedTime,"
-			
+
 			+ " pidoc.id as pidocId, pidoc.tenantId as pidoctenantid, pidoc.documenttype as pidoctype , pidoc.filestoreid as pidoc_filestoreid,"
 			+ " pidoc.property_images_application_id as pidoc_piid , pidoc.active as pidoc_active "
 
-			+ " FROM cs_pt_property_v1 pt " + INNER_JOIN 
-			+ " cs_pt_propertydetails_v1 ptdl ON pt.id =ptdl.property_id "+ INNER_JOIN 
-			+ " cs_pt_ownership_v1 ownership ON pt.id=ownership.property_id " + LEFT_JOIN
+			+ " FROM cs_pt_property_v1 pt " + INNER_JOIN + " cs_pt_propertydetails_v1 ptdl ON pt.id =ptdl.property_id "
+			+ INNER_JOIN + " cs_pt_ownership_v1 ownership ON pt.id=ownership.property_id " + LEFT_JOIN
 			+ " cs_pt_ownershipdetails_v1 od ON ownership.id = od.owner_id " + LEFT_JOIN
 			+ " cs_pt_address_v1 address ON pt.id=address.property_id " + LEFT_JOIN
 			+ " cs_pt_application_documents_v1 doc ON pt.id=doc.property_id " + LEFT_JOIN
-			+ " cs_pt_property_images_application pi ON pi.propertyid = pt.id " + LEFT_JOIN 
+			+ " cs_pt_property_images_application pi ON pi.propertyid = pt.id " + LEFT_JOIN
 			+ " cs_pt_property_images_document pidoc ON pidoc.property_images_application_id =  pi.id"
 //			+ " WHERE "
 	;
--- cs-property-service/src/main/java/org/egov/cpt/service/EnrichmentService.java
+++ cs-property-service/src/main/java/org/egov/cpt/service/EnrichmentService.java
@@ -458,8 +458,8 @@ public class EnrichmentService {
 		}
 		setDCIdgenIds(duplicateCopyRequest);
 	}
-	
-	//PI Enrich
+
+	// PI Enrich
 	public void enrichpropertyImageCreateRequest(PropertyImagesRequest propertyImagesRequest) {
 		RequestInfo requestInfo = propertyImagesRequest.getRequestInfo();
 		AuditDetails propertyAuditDetails = propertyutil.getAuditDetails(requestInfo.getUserInfo().getUuid(), true);
@@ -473,7 +473,7 @@ public class EnrichmentService {
 				application.setAuditDetails(propertyAuditDetails);
 				System.out.println(propertyAuditDetails.toString() + " audit details here");
 
-				//Document details
+				// Document details
 				if (!CollectionUtils.isEmpty(application.getApplicationDocuments())) {
 					application.getApplicationDocuments().forEach(document -> {
 						if (document.getId() == null) {
@@ -490,7 +490,7 @@ public class EnrichmentService {
 					});
 
 				}
-				
+
 			});
 		}
 		setPIIdgenIds(propertyImagesRequest);
@@ -562,15 +562,16 @@ public class EnrichmentService {
 		});
 
 	}
-	
-	//PI IDGen
+
+	// PI IDGen
 	private void setPIIdgenIds(PropertyImagesRequest propertyImagesRequest) {
 		RequestInfo requestInfo = propertyImagesRequest.getRequestInfo();
 		String tenantId = propertyImagesRequest.getPropertyImagesApplications().get(0).getTenantId();
 		List<PropertyImages> applications = propertyImagesRequest.getPropertyImagesApplications();
 		List<String> applicationNumbers = null;
 		applicationNumbers = getIdList(requestInfo, tenantId, config.getApplicationNumberIdgenNamePI(),
-				config.getApplicationNumberIdgenFormatPI(), propertyImagesRequest.getPropertyImagesApplications().size());
+				config.getApplicationNumberIdgenFormatPI(),
+				propertyImagesRequest.getPropertyImagesApplications().size());
 		ListIterator<String> itr = applicationNumbers.listIterator();
 
 		Map<String, String> errorMap = new HashMap<>();
@@ -625,8 +626,8 @@ public class EnrichmentService {
 		}
 
 	}
-	
-	//PI Update Enrich
+
+	// PI Update Enrich
 	public void enrichpropertyImagesUpdateRequest(PropertyImagesRequest propertyImagesRequest,
 			List<PropertyImages> searchedProperty) {
 		RequestInfo requestInfo = propertyImagesRequest.getRequestInfo();
@@ -763,21 +764,19 @@ public class EnrichmentService {
 
 			properties.forEach(property -> {
 				property.getPropertyDetails().setCurrentOwner(latestOwner.getOwnerDetails().getName());
+
 				property.getOwners().forEach(existingOwner -> {
 					if (existingOwner.getId().contentEquals(latestOwner.getId())) {
 						latestOwner.setActiveState(true);
-						latestOwner.getOwnerDetails().setAllotmentStartdate(getCurrentTimeEpoch());
 						latestOwner.getOwnerDetails().setPermanent(true);
-//						latestOwner.setIsPrimaryOwner("true");
-//						latestOwner.getOwnerDetails().setAllotmentEnddate(allotmentEnddate);
+						latestOwner.getOwnerDetails().setAllotmentStartdate(getCurrentTimeEpoch());
 					} else if (!existingOwner.getId().contentEquals(latestOwner.getId())) {
 						existingOwner.setActiveState(false);
 						existingOwner.getOwnerDetails().setPermanent(false);
-//						existingOwner.setIsPrimaryOwner("false");
-//						existingOwner.getOwnerDetails().setAllotmentEnddate(currentDate);
 					}
 				});
 			});
+
 			PropertyRequest propertyRequest = new PropertyRequest();
 			propertyRequest.setRequestInfo(ownershipTransferRequest.getRequestInfo());
 			propertyRequest.setProperties(properties);
