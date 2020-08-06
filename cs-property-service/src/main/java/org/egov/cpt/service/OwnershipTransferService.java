cb73575607cdcb3452d7c8360c43f4b2c8fc5db1
sunitha <sunitha.ranjith@transerve.com>
2020-08-06 19:03:02 +0530

    demand modification

--- cs-property-service/src/main/java/org/egov/cpt/service/OwnershipTransferService.java
+++ cs-property-service/src/main/java/org/egov/cpt/service/OwnershipTransferService.java
@@ -57,7 +57,6 @@ public class OwnershipTransferService {
 //		propertyValidator.validateCreateRequest(request); // TODO add validations as per requirement
 		List<Property> propertyFromSearch = propertyValidator.getPropertyForOT(request);
 		enrichmentService.enrichCreateOwnershipTransfer(request, propertyFromSearch);
-		demandService.createDemand(request.getRequestInfo(), request.getOwners());
 		if (config.getIsWorkflowEnabled()) {
 			wfIntegrator.callOwnershipTransferWorkFlow(request);
 		}
@@ -95,11 +94,11 @@ public class OwnershipTransferService {
 		List<Owner> ownersFromSearch = propertyValidator.validateUpdateRequest(request);
 		enrichmentService.enrichUpdateOwnershipTransfer(request, ownersFromSearch);
 		String applicationState = request.getOwners().get(0).getApplicationState(); // demand generation
-		if (applicationState.equalsIgnoreCase(PTConstants.OT_STATE_PENDING_SA_VERIFICATION)) {
+		/*if (applicationState.equalsIgnoreCase(PTConstants.OT_STATE_PENDING_SA_VERIFICATION)) {
 			demandService.updateDemand(request.getRequestInfo(), request.getOwners());
-		}
+		}*/
 		if (applicationState.equalsIgnoreCase(PTConstants.OT_STATE_PENDING_APRO)) {
-			demandService.updateDemand(request.getRequestInfo(), request.getOwners());
+			demandService.generateDemand(request.getRequestInfo(), request.getOwners());
 		}
 		if (config.getIsWorkflowEnabled()) {
 			wfIntegrator.callOwnershipTransferWorkFlow(request);
