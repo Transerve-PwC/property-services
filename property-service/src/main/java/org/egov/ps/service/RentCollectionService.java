99b056233a564559b93edbc8a8312130a282b29d
unknown <Itmusketeers0310@gmail.com>
2020-08-10 19:47:47 +0530

    Test case complete for test calculation

--- property-service/src/main/java/org/egov/ps/service/RentCollectionService.java
+++ property-service/src/main/java/org/egov/ps/service/RentCollectionService.java
@@ -1,6 +1,7 @@
 package org.egov.ps.service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.egov.ps.model.RentCollection;
@@ -12,6 +13,40 @@ import org.springframework.stereotype.Service;
 public class RentCollectionService {
 
 	public List<RentCollection> getCollectionsForPayment(ArrayList<RentDemand> demands, RentPayment payment) {
-		return null;
+		double interestRate = 24;
+		Date paymentDate = payment.getDateOfPayment();
+		Double paybleAmount = payment.getAmountPaid();
+		ArrayList<RentCollection> collections = new ArrayList<RentCollection>();
+		
+		for (RentDemand rentDemand: demands) {
+			RentCollection rentalCollection = new RentCollection();			
+			Date generationDate = rentDemand.getGenerationDate();
+
+			float daysBetween = ((paymentDate.getTime() - generationDate.getTime()) / (1000 * 60 * 60 * 24)) + 1;
+			if (daysBetween > rentDemand.getInitialGracePeriod()) {
+				double interest = ((rentDemand.getCollectionPrincipal() * interestRate) / 100) * (daysBetween / 365);
+				
+				if (interest < paybleAmount) {
+					rentalCollection.setInterestCollected(interest);
+					paybleAmount = paybleAmount - interest;
+
+				} else {
+					rentalCollection.setInterestCollected(paybleAmount);
+					paybleAmount = 0.0;
+				}
+			} else
+				rentalCollection.setInterestCollected(0.0);
+
+			if (rentDemand.getCollectionPrincipal() < paybleAmount) {
+				rentalCollection.setPrincipalCollected(rentDemand.getCollectionPrincipal());
+				paybleAmount = paybleAmount - rentDemand.getCollectionPrincipal();
+			} else {
+				rentalCollection.setPrincipalCollected(paybleAmount);
+				paybleAmount = 0.0;
+			}
+			collections.add(rentalCollection);
+		}
+		return collections;
+
 	}
 }
