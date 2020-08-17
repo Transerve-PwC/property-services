e1251bb4863cb4fb5807fd508e66717767917908
sunitha <sunitha.ranjith@transerve.com>
2020-08-17 13:12:39 +0530

    changes from get to post

--- cs-property-service/src/main/java/org/egov/cpt/util/FileStoreUtils.java
+++ cs-property-service/src/main/java/org/egov/cpt/util/FileStoreUtils.java
@@ -3,6 +3,7 @@ package org.egov.cpt.util;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.egov.cpt.models.ExcelSearchCriteria;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
@@ -25,13 +26,13 @@ public class FileStoreUtils {
 	}
 
 	@Cacheable(value = "fileUrl", sync = true)
-	public String fetchFileStoreUrl(String tenantId, String fileStoreIds) {
+	public String fetchFileStoreUrl(ExcelSearchCriteria searchCriteria) {
 		String responseMap ="";
 		StringBuilder uri = new StringBuilder(fileStoreUrl);
-		uri.append("?tenantId="+tenantId+"&fileStoreIds="+fileStoreIds);
+		uri.append("?tenantId="+searchCriteria.getTenantId()+"&fileStoreIds="+searchCriteria.getFileStoreId());
 		try {
 			Map<String,Object> response = restTemplate.getForObject(uri.toString(), HashMap.class);
-			responseMap = String.valueOf(response.get(fileStoreIds));
+			responseMap = String.valueOf(response.get(searchCriteria.getFileStoreId()));
 		} catch (Exception e) {
 			log.error("Exception while fetching file url: ", e);
 		}
