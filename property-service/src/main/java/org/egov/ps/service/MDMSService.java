9aa73a4ec516d83a83bb77a1816b7967794ae2be
Vinil Kumar Bhandekar <vinil.b@transerve.com>
2020-08-18 20:11:26 +0530

    mdms valiations with code reuse

--- property-service/src/main/java/org/egov/ps/service/MDMSService.java
+++ property-service/src/main/java/org/egov/ps/service/MDMSService.java
@@ -35,12 +35,11 @@ public class MDMSService {
 //		TODO: change in mdms-data config
 		tenantId = tenantId.split("\\.")[0];
 		MdmsCriteriaReq mdmsCriteriaReq = util.prepareMdMsRequest(tenantId, PSConstants.MDMS_PS_MODULE_NAME,
-				Arrays.asList(PSConstants.MDMS_PS_FIELDS), null, requestInfo);
-		StringBuilder url = getMdmsSearchUrl(tenantId, applicationType);
+				Arrays.asList(applicationType), PSConstants.MDMS_PS_FIELD_FILTER, requestInfo);
+		StringBuilder url = getMdmsSearchUrl(tenantId, applicationType, PSConstants.MDMS_PS_MODULE_NAME);
 		Object response = serviceRequestRepository.fetchResult(url, mdmsCriteriaReq);
 
-		String MDMSResponsePath = "$.MdmsRes." + PSConstants.MDMS_PS_MODULE_NAME + "." + applicationType
-				+ ".*.fields.*";
+		String MDMSResponsePath = "$.MdmsRes." + PSConstants.MDMS_PS_MODULE_NAME + "." + applicationType;
 
 		List<Map<String, Object>> fieldConfigurations = JsonPath.read(response, MDMSResponsePath);
 		return fieldConfigurations;
@@ -51,10 +50,27 @@ public class MDMSService {
 	 *
 	 * @return MDMS Search URL
 	 */
-	private StringBuilder getMdmsSearchUrl(@NotNull final String tenantId, @NotNull final String masterName) {
+	public StringBuilder getMdmsSearchUrl(@NotNull final String tenantId, @NotNull final String moduleName, @NotNull final String masterName) {
 		return new StringBuilder().append(config.getMdmsHost()).append(config.getMdmsSearchEndpoint())
-				.append("?tenantId=").append(tenantId).append("&moduleName=").append(PSConstants.MDMS_PS_MODULE_NAME)
+				.append("?tenantId=").append(tenantId).append("&moduleName=").append(moduleName)
 				.append("&masterName=").append(masterName);
+	}
+
+	public List<String> getMdmsFields(String moduleName, String masterName, String filter) {
+		String tenantId = PSConstants.TENENT_ID;
+		RequestInfo requestInfo = RequestInfo.builder().authToken("authToken").build();
+
+		tenantId = tenantId.split("\\.")[0];
+		MdmsCriteriaReq mdmsCriteriaReq = util.prepareMdMsRequest(tenantId, moduleName, Arrays.asList(masterName),
+				PSConstants.MDMS_PS_CODE_FILTER, requestInfo);
+		StringBuilder url = getMdmsSearchUrl(tenantId, moduleName, masterName);
+		Object response = serviceRequestRepository.fetchResult(url, mdmsCriteriaReq);
+		
+		String MDMSResponsePath = "$.MdmsRes." + moduleName + "." + masterName;
+
+		List<String> allowedValues = JsonPath.read(response, MDMSResponsePath);
+		
+		return allowedValues;
 	}
 
 }
--- property-service/src/main/java/org/egov/ps/validator/application/MDMSValidator.java
+++ property-service/src/main/java/org/egov/ps/validator/application/MDMSValidator.java
@@ -4,74 +4,36 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import javax.validation.constraints.NotNull;
-
 import org.apache.commons.lang.StringUtils;
-import org.egov.common.contract.request.RequestInfo;
-import org.egov.mdms.model.MdmsCriteriaReq;
 import org.egov.ps.annotation.ApplicationValidator;
-import org.egov.ps.config.Configuration;
-import org.egov.ps.repository.ServiceRequestRepository;
 import org.egov.ps.service.MDMSService;
-import org.egov.ps.util.PSConstants;
-import org.egov.ps.util.Util;
 import org.egov.ps.validator.IApplicationField;
 import org.egov.ps.validator.IApplicationValidator;
 import org.egov.ps.validator.IValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.jayway.jsonpath.JsonPath;
-
 @ApplicationValidator("mdms")
 @Component
 public class MDMSValidator implements IApplicationValidator {
 
 	@Autowired
-	Configuration config;
-
-	@Autowired
 	MDMSService mdmsService;
-	
-	@Autowired
-	ServiceRequestRepository serviceRequestRepository;
-
-	@Autowired
-	private Util util;
 
 	@Override
 	public List<String> validate(IValidation validation, IApplicationField field, Object value, Object parent) {
 		Map<String, Object> params = validation.getParams();
 		String moduleName = (String) params.get("moduleName");
 		String masterName = (String) params.get("masterName");
+		String filter = (String) params.get("filter");
 
-		String tenantId = PSConstants.TENENT_ID;
-		RequestInfo requestInfo = RequestInfo.builder().authToken("authToken").build();
-
-		tenantId = tenantId.split("\\.")[0];
-		MdmsCriteriaReq mdmsCriteriaReq = util.prepareMdMsRequest(tenantId, moduleName, Arrays.asList(masterName),
-				PSConstants.MDMS_PS_PATH_FILTER, requestInfo);
-		StringBuilder url = getMdmsSearchUrl(tenantId, moduleName, masterName);
-		Object response = serviceRequestRepository.fetchResult(url, mdmsCriteriaReq);
-		
-//		List<Map<String, Object>> fieldConfigurations = mdmsService.getApplicationConfig(moduleName, requestInfo, tenantId);
-
-		String MDMSResponsePath = "$.MdmsRes." + moduleName + "." + masterName;
-
-		List<String> allowedValues = JsonPath.read(response, MDMSResponsePath);
+		List<String> allowedValues = mdmsService.getMdmsFields(moduleName, masterName, filter);
 
 		if (!allowedValues.stream().filter(allowedValue -> allowedValue.equals(value)).findAny().isPresent()) {
 			return Arrays.asList(String.format("Value '%s' not found in expected mdms values [%s] for path '%s'", value,
 					StringUtils.join(allowedValues, ","), field.getPath()));
 		}
 		return null;
-	}
-
-	private StringBuilder getMdmsSearchUrl(@NotNull final String tenantId, @NotNull final String moduleName,
-			@NotNull final String masterName) {
-		return new StringBuilder().append(config.getMdmsHost()).append(config.getMdmsSearchEndpoint())
-				.append("?tenantId=").append(tenantId).append("&moduleName=").append(moduleName).append("&masterName=")
-				.append(masterName);
 	}
 
 }
