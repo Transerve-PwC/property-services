630a084c78bf7e8396a60ae17ffe56ae75216b69
Rajiv Narayana <rajiv.s@transerve.com>
2020-08-24 19:36:51 +0530

    Refactor validateDocuments to be reusable. Created MDMSService.

--- cs-property-service/src/main/java/org/egov/cpt/validator/PropertyValidator.java
+++ cs-property-service/src/main/java/org/egov/cpt/validator/PropertyValidator.java
@@ -24,6 +24,7 @@ import org.egov.cpt.models.calculation.State;
 import org.egov.cpt.repository.OwnershipTransferRepository;
 import org.egov.cpt.repository.PropertyRepository;
 import org.egov.cpt.repository.ServiceRequestRepository;
+import org.egov.cpt.service.MDMSService;
 import org.egov.cpt.util.DuplicateCopyConstants;
 import org.egov.cpt.util.PTConstants;
 import org.egov.cpt.util.PropertyUtil;
@@ -63,6 +64,9 @@ public class PropertyValidator {
 
 	@Autowired
 	private WorkflowService workflowService;
+
+	@Autowired
+	private MDMSService mdmsService;
 
 	@Value("${egov.mdms.host}")
 	private String mdmsHost;
@@ -231,7 +235,7 @@ public class PropertyValidator {
 			Object result = serviceRequestRepository.fetchResult(uri, criteriaReq);
 			return JsonPath.read(result, jsonpath);
 		} catch (Exception e) {
-//			log.error("Error while fetching MDMS data", e);
+			log.error("Error while fetching MDMS data", e);
 			throw new CustomException("INVALID TENANT ID ", "No data found for this tenentID");
 		}
 
@@ -300,7 +304,8 @@ public class PropertyValidator {
 		validateColony(request, errorMap);
 		validateArea(request, errorMap);
 
-		validateDocuments(request, errorMap);
+		validatePropertyDocuments(request, errorMap);
+
 		// TODO Commenting for temporary. Uncomment for payment validations
 //		validatePayment(request, errorMap);
 
@@ -1022,33 +1027,35 @@ public class PropertyValidator {
 			.forEach(documents -> _validateDuplicateDocuments(documents));
 	}
 
-	private void validateDocuments(PropertyRequest request, Map<String, String> errorMap) {
 
-		String tenantId = request.getProperties().get(0).getTenantId();
-		RequestInfo requestInfo = request.getRequestInfo();
-
-		String filter = "[?(@.code=='MasterRP')].documentList..code";
-		Map<String, List<String>> documents = getAttributeValues(tenantId.split("\\.")[0],
-				PTConstants.MDMS_PT_EGF_PROPERTY_SERVICE, Arrays.asList("applications"), filter,
-				"$.MdmsRes.PropertyServices", requestInfo);
-		List<String> type = new ArrayList<>();
+	private void validatePropertyDocuments(PropertyRequest request, Map<String, String> errorMap) {
 		request.getProperties().forEach(property -> {
+			this.validateDocumentsOnType(request.getRequestInfo(), property.getTenantId(), property.getPropertyDetails().getApplicationDocuments(), errorMap, PTConstants.BUSINESS_SERVICE_PM );
+		});
+	}
 
-			property.getPropertyDetails().getApplicationDocuments().forEach(doc -> {
-				type.add(doc.getDocumentType());
-			});
+	private void validateDocumentsOnType(RequestInfo requestInfo, String tenantId, List<Document> documents, Map<String, String> errorMap, String code) {
 
-			documents.entrySet().stream().forEach(obj -> {
-				List<String> l = obj.getValue();
-				for (int i = 0; i < l.size(); i++) {
-					if (!type.contains(l.get(i))) {
-						errorMap.put("INVALID NUMBER OF DOCUMENTS" + i,
-								"The document '" + l.get(i) + "' is not present");
-					}
+		tenantId = tenantId.split("\\.")[0];
+		String filter = "[?(@.code=='" + code + "')].documentList";
+
+		/**
+		 * Get list of application documents from MDMS
+		 */
+		List<Map<String, Object>> data = (List<Map<String, Object>>)mdmsService.getMDMSResponse(requestInfo, tenantId, PTConstants.MDMS_PT_EGF_PROPERTY_SERVICE, 
+			"applications", filter, "$.MdmsRes.PropertyServices.applications");
+		List<Map<String, Object>> mdmsDocuments = (List<Map<String, Object>>)(data.get(0));
+
+		/**
+		 * Filter the mdms document types for the ones that are required.
+		 * For each required document code, verify if it is present in the incoming documents.
+		 */
+		mdmsDocuments.stream()
+			.filter(d -> ((boolean)(d.get("required"))))
+			.map(d -> d.get("code")).forEach(documentCode -> {
+				if (!documents.stream().map(Document::getDocumentType).filter(type -> type.equalsIgnoreCase(String.valueOf(documentCode))).findAny().isPresent()) {
+					errorMap.put("REQUIRED DOCUMENT NOT FOUND", "The document type '" + documentCode + "' is required but it is not present");
 				}
 			});
-
-		});
-
 	}
 }
