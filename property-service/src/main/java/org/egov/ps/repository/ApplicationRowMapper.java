85959828ee9e4664281dae9c81e528579352b73f
Sri Raj <sriraj.a@transerve.com>
2020-10-05 00:28:00 +0530

    Owners serch is now giving list of owners instead of single owner.

--- property-service/src/main/java/org/egov/ps/repository/ApplicationRowMapper.java
+++ property-service/src/main/java/org/egov/ps/repository/ApplicationRowMapper.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.egov.ps.model.Application;
 import org.egov.ps.model.Document;
+import org.egov.ps.model.Owner;
 import org.egov.ps.model.OwnerDetails;
 import org.egov.ps.model.Property;
 import org.egov.ps.model.PropertyDetails;
@@ -66,6 +67,46 @@ public class ApplicationRowMapper implements ResultSetExtractor<List<Application
 						}
 					}
 					applicationMap.put(applicationId, currentApplication);
+
+					if (hasColumn(rs, "pid")) {
+						try {
+							if (rs.getString("pid") != null) {
+
+								final String PropertyDetailId = rs.getString("ptdlid");
+
+								PropertyDetails propertyDetails = PropertyDetails.builder().id(PropertyDetailId)
+										.propertyId(rs.getString("pdproperty_id"))
+										.propertyType(rs.getString("pdproperty_type"))
+										.tenantId(rs.getString("pttenantid"))
+										.typeOfAllocation(rs.getString("type_of_allocation"))
+										.modeOfAuction(rs.getString("mode_of_auction"))
+										.schemeName(rs.getString("scheme_name")).areaSqft(rs.getInt("area_sqft"))
+										.dateOfAuction(rs.getLong("date_of_auction"))
+										.ratePerSqft(rs.getBigDecimal("rate_per_sqft"))
+										.lastNocDate(rs.getLong("last_noc_date"))
+										.serviceCategory(rs.getString("service_category"))
+										.isPropertyActive(rs.getBoolean("is_property_active"))
+										.tradeType(rs.getString("trade_type")).companyName(rs.getString("company_name"))
+										.companyAddress(rs.getString("company_address"))
+										.companyRegistrationNumber(rs.getString("company_registration_number"))
+										.companyType(rs.getString("company_type"))
+										.emdAmount(rs.getBigDecimal("emd_amount")).emdDate(rs.getLong("emd_date"))
+										.build();
+
+								Property property = Property.builder().id(rs.getString("pid"))
+										.fileNumber(rs.getString("file_number")).tenantId(rs.getString("pttenantid"))
+										.category(rs.getString("category")).subCategory(rs.getString("sub_category"))
+										.sectorNumber(rs.getString("sector_number"))
+										.siteNumber(rs.getString("site_number")).state(rs.getString("state"))
+										.action(rs.getString("action")).propertyDetails(propertyDetails).build();
+
+								currentApplication.setProperty(property);
+
+							}
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}
+					}
 				}
 			}
 			addChildrenToApplication(rs, currentApplication, applicationMap);
@@ -95,74 +136,6 @@ public class ApplicationRowMapper implements ResultSetExtractor<List<Application
 							.fileStoreId(rs.getString("file_store_id")).propertyId(rs.getString("docproperty_id"))
 							.auditDetails(docAuditdetails).build();
 					currentApplication.addApplicationDocumentsItem(applicationDocuments);
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-//		List<Owner> ownersList = new ArrayList<Owner>();
-//		if (hasColumn(rs, "oid")) {
-//			final String ownerId = rs.getString("oid");
-//			final String ownerDetailId = rs.getString("odid");
-//			final String OwnerPropertyDetailId = rs.getString("oproperty_details_id");
-//
-//			if (ownerId != null) {
-//
-//				final OwnerDetails ownerDetails = OwnerDetails.builder().id(ownerDetailId)
-//						.ownerId(rs.getString("odowner_id")).ownerName(rs.getString("odowner_name"))
-//						.tenantId(rs.getString("otenantid")).guardianName(rs.getString("guardian_name"))
-//						.guardianRelation(rs.getString("guardian_relation")).mobileNumber(rs.getString("mobile_number"))
-//						.allotmentNumber(rs.getString("allotment_number"))
-//						.dateOfAllotment(rs.getLong("date_of_allotment")).possesionDate(rs.getLong("possesion_date"))
-//						.isApproved(rs.getBoolean("is_approved")).isCurrentOwner(rs.getBoolean("is_current_owner"))
-//						.isMasterEntry(rs.getBoolean("is_master_entry")).address(rs.getString("address"))
-//						.isDirector(rs.getBoolean("is_director")).build();
-//
-//				Owner owners = Owner.builder().id(ownerId).propertyDetailsId(OwnerPropertyDetailId)
-//						.tenantId(rs.getString("otenantid")).serialNumber(rs.getString("oserial_number"))
-//						.share(rs.getDouble("oshare")).cpNumber(rs.getString("ocp_number"))
-//						.ownershipType(rs.getString("ownership_type")).ownerDetails(ownerDetails).build();
-//
-//				ownersList.add(owners);
-//			}
-//		}
-
-		if (hasColumn(rs, "pid")) {
-			try {
-				if (rs.getString("pid") != null) {
-
-					final String PropertyDetailId = rs.getString("ptdlid");
-					
-					
-
-					PropertyDetails propertyDetails = PropertyDetails.builder().id(PropertyDetailId)
-							.propertyId(rs.getString("pdproperty_id")).propertyType(rs.getString("pdproperty_type"))
-							.tenantId(rs.getString("pttenantid")).typeOfAllocation(rs.getString("type_of_allocation"))
-							.modeOfAuction(rs.getString("mode_of_auction")).schemeName(rs.getString("scheme_name"))
-							.areaSqft(rs.getInt("area_sqft")).dateOfAuction(rs.getLong("date_of_auction"))
-							.ratePerSqft(rs.getBigDecimal("rate_per_sqft")).lastNocDate(rs.getLong("last_noc_date"))
-							.serviceCategory(rs.getString("service_category"))
-							.isPropertyActive(rs.getBoolean("is_property_active")).tradeType(rs.getString("trade_type"))
-							.companyName(rs.getString("company_name")).companyAddress(rs.getString("company_address"))
-							.companyRegistrationNumber(rs.getString("company_registration_number"))
-							.companyType(rs.getString("company_type")).emdAmount(rs.getBigDecimal("emd_amount"))
-							.emdDate(rs.getLong("emd_date")).build();
-
-					Property property = Property.builder().id(rs.getString("pid"))
-							.fileNumber(rs.getString("file_number")).tenantId(rs.getString("pttenantid"))
-							.category(rs.getString("category")).subCategory(rs.getString("sub_category"))
-							.sectorNumber(rs.getString("sector_number")).siteNumber(rs.getString("site_number"))
-							.state(rs.getString("state")).action(rs.getString("action"))
-							.propertyDetails(propertyDetails).build();
-
-					currentApplication.setProperty(property);
-					
-//					for (Property owner : currentApplication.getProperty()) {
-//						if (owner.getId().equalsIgnoreCase(rs.getString("oid"))) {
-//							owner.addApplicationDocumentsItem(applicationDocument);
-//						}
-//					}
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
